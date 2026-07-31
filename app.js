9df3802f3dfa0c1589b091d9a679b6f9a5ab6c6d
Asis <Asis@gmail.com>
2026-07-31 16:40:07 -0500

    feat: endpoints varios

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 import express from 'express';
+import "dotenv/config";
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
-const port = 3000;
+const port = process.env.PUERTO;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rutaImagen = path.join(__dirname, 'images.jpg');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`<h1>IDK<h1>`);
 });
+
+app.get('/bart', (req, res) => {
+  res.sendFile(rutaImagen);
+});
+
+app.get('/route1', (req,res) => {
+  res.send(`<h1>Respuesta res<h1>`)
+})
+
+app.get('/route2', (req,res) => {
+  res.json({"nombre" : "camilo", "edad" : "17"})
+})
+
+app.get('/route3/:nombre/:edad', (req,res) => {
+  let nombreUsuario = req.params.nombre
+  let edadUsuario = req.params.edad
+  res.json({"nombre" : nombreUsuario, "edad" : edadUsuario})
+})
+
+app.get('/route4', (req,res) => {
+  let numero = req.query.phone || 3118471307
+  let orden = req.query.orden || "sin orden"
+  let pagina = req.query.pagina || 1
+  res.send(`<h1>Listado aprendices</h1>
+    <h2> listado en orden ${orden} </h2>
+    <p> pagina ${pagina} <p>
+    <h3> Numero ${numero} </h3> `)
+})
 
 app.listen(port, () => {
   console.log(`Escuchando puerto : ${port}`);
